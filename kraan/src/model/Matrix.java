f7df1b8514277a1f480fefccf3a14ddce29f86bc
Pieter-Jan Robrecht <pieterjan.robrecht@student.kuleuven.be>
2016-10-14 10:27:56 +0200

    matrix constructor + getters/setters

--- kraan/src/model/Matrix.java
+++ kraan/src/model/Matrix.java
@@ -1,5 +1,29 @@
 package model;
 
+import java.util.List;
+
+import kraan.Slot;
+
+//Matrix kan niet overweg met geschrankte containers
 public class Matrix {
 	
+	//Met als conventie X Y Z een rechtsdraaiend assenstelsel
+	private Slot[][][] park;
+
+	public Matrix() {
+		super();
+	}
+
+	public Matrix(List<Slot> slots, int maxXValue, int maxYValue, int maxZValue) {
+		super();
+		park = new Slot[maxXValue/10][maxYValue/10][maxZValue];
+	}
+
+	public Slot[][][] getPark() {
+		return park;
+	}
+
+	public void setPark(Slot[][][] park) {
+		this.park = park;
+	}
 }
