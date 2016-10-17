da80fc46bb417d29958ed6509531f99fbf177715
Tech2Logix <rhinovanboxelaere@outlook.com>
2016-10-17 18:03:54 +0200

    HET WERKT LEL HEHEXD

--- kraan/src/kraan/Main.java
+++ kraan/src/kraan/Main.java
@@ -15,17 +15,43 @@ public class Main {
 		 
         try {
 
-        	//File file = new File("./data/1_50_50_10_FALSE_60_25_100.json");
-//			File file = new File ("C:/Users/Pieter-Jan/Documents/eclipse/AlgoritmenBeslissingssytemen/kraan/data/testInput.json");
-			File file = new File("./data/testInput.json");
+        	File file = new File("./data/1_50_50_10_FALSE_60_25_100.json");
+			//File file = new File ("C:/Users/Pieter-Jan/Documents/eclipse/AlgoritmenBeslissingssytemen/kraan/data/testInput.json");
+			//File file = new File("./data/testInput.json");
         	
         	Problem uwProbleem = Problem.fromJson(file);
  
         	
         	Yard y = new Yard(uwProbleem);
-        	y.printOutYard();
-        	y.printHash();
+        	//y.printOutYard();
+        	//y.printHash();
         	
+        	int inLimit = uwProbleem.getInputJobSequence().size();
+        	int outLimit = uwProbleem.getOutputJobSequence().size();
+        	int limit = Math.max(inLimit, outLimit);
+        	for(int i = 0; i < limit; i++) {
+        		//input job:
+        		if(i<inLimit) {
+        			y.executeJob(uwProbleem.getInputJobSequence().get(i), "INPUT");
+                	//y.printOutYard();
+        		}
+        		//output job:
+        		if(i<outLimit) {
+        			y.executeJob(uwProbleem.getOutputJobSequence().get(i), "OUTPUT");
+                	//y.printOutYard();
+        		}
+        	}
+        	System.out.println("Done!");
+        	System.out.println("Tasks in backlog IN (" + y.getBacklogIN().size()+"): " + y.getBacklogIN().toString());
+        	System.out.println("Tasks in backlog OUT (" + y.getBacklogOUT().size()+"): " + y.getBacklogOUT().toString());
+        	System.out.println("\n\n\n\n");
+        	for(Job j : y.getBacklogIN()) {
+        		y.executeJob(j, "INPUT");
+        	}
+        	
+        	for(Job j : y.getBacklogOUT()) {
+        		y.executeJob(j, "OUTPUT");
+        	}
         } catch (Exception e) {
             e.printStackTrace();
         }
--- kraan/src/model/Yard.java
+++ kraan/src/model/Yard.java
@@ -1,5 +1,6 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -14,6 +15,8 @@ public class Yard {
 	private HashMap<Integer, Slot> itemIDList = new HashMap<Integer, Slot>();
 	private List<Slot> slotList;
 	private Slot inputSlot, outputSlot;
+	private List<Job> backlogOUT = new ArrayList<>();
+	private List<Job> backlogIN = new ArrayList<>();
 
 	public Yard(Problem probleem) {
 		System.out.println("Yard initiating..");
@@ -69,6 +72,14 @@ public class Yard {
 		this.outputSlot = outputSlot;
 	}
 
+	public List<Job> getBacklogIN() {
+		return backlogIN;
+	}
+
+	public void setBacklogIN(List<Job> backlogIN) {
+		this.backlogIN = backlogIN;
+	}
+
 	public void initializeYard(Problem probleem) {
 		this.height = probleem.getMaxLevels();
 		this.width = probleem.getMaxY() / 10;
@@ -92,14 +103,14 @@ public class Yard {
 				// Math.floor(s.getXMin() / 10) + " | "+ s.getXMin() / 10);
 				if (Math.floor(s.getXMin() / 10) != s.getXMin() / 10) {
 					// ([yCoords*length+xCoords]-yCoords) ZIE EXCEL
-					System.out.println("DEBUG (STAGGERED) - [" + (width * length + yCoords * (length - 1) + xCoords)
-							+ "] || [" + s.getZ() + "]");
+					//System.out.println("DEBUG (STAGGERED) - [" + (width * length + yCoords * (length - 1) + xCoords)
+					//		+ "] || [" + s.getZ() + "]");
 					yard[(width * length - 1) + (yCoords * (length - 1) + (xCoords + 1))][s.getZ()] = s;
 					if (s.getItem() != null)
 						itemIDList.put(s.getItem().getId(), s);
 				} else { // not staggered
-					System.out.println("DEBUG - [ yCoord: " + yCoords + " xCoords: " + xCoords + " -> "
-							+ (yCoords * length + xCoords) + "] || [" + s.getZ() + "]");
+					//System.out.println("DEBUG - [ yCoord: " + yCoords + " xCoords: " + xCoords + " -> "
+					//		+ (yCoords * length + xCoords) + "] || [" + s.getZ() + "]");
 					yard[yCoords * length + xCoords][s.getZ()] = s;
 					if (s.getItem() != null)
 						itemIDList.put(s.getItem().getId(), s);
@@ -120,9 +131,17 @@ public class Yard {
 			return null;
 		}
 	}
+	
+	public List<Job> getBacklogOUT() {
+		return backlogOUT;
+	}
+
+	public void setBacklogOUT(List<Job> backlog) {
+		this.backlogOUT = backlog;
+	}
 
 	public void printOutYard() {
-		for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < 2; i++) {
 			System.out.println();
 		}
 		System.out.println("Visualizing yard!");
@@ -150,60 +169,72 @@ public class Yard {
 
 	public boolean addItem(Item i) {
 		boolean found = false;
-
 		for (int j = 0; j < slotList.size(); j++) {
 			Slot temp = slotList.get(j);
 			if (temp.getItem() == null) {
-				System.out.println("DEBUG - We found a suiteable slot!");
 				temp.setItem(i);
-				j = slotList.size() + 10;
+				itemIDList.put(i.getId(), temp);
+				System.out.println("DEBUG - We found a suiteable slot! Placing item " + i.toString() + " in "+ temp.toString());
 				found = true;
+				j = slotList.size() + 10;
 			}
 		}
-
 		return found;
 	}
 
 	public boolean digItem(Item i) {
 		boolean succes = false;
-		Slot core = itemIDList.get(i.getId());
-		if (maakVrij(core)) {
-			// mogen nu vrij bewegen!
-			core.setItem(null); // --> zogezegd naar eindslot gemoved en
-								// verwijdert uit yard
-		}
+		
+			System.out.println("DEBUG - Item info: " + i.toString());
+			Slot core = itemIDList.get(i.getId());
+			if(core == null) {
+				System.out.println("ERRORDEBUG - " + i.toString());
+			} else {
+				if (maakVrij(core)) {
+					// mogen nu vrij bewegen!
+					succes = true;
+					//itemIDList.remove(core.getItem().getId());
+					core.setItem(null); // --> zogezegd naar eindslot gemoved en
+										// verwijdert uit yard
+
+				}
+			}
 		return succes;
 	}
 
 	public boolean maakVrij(Slot s) {
+		System.out.println("DEBUG - Trying to empty slot.. " + s.toString());
 		boolean vrij = false;
-
 		if (s.getZ() < height) {
 			int xCoords = s.getCenterX() / 10;
 			int yCoords = s.getCenterY() / 10;
 
-			if (Math.floor(s.getXMin() / 10) != s.getXMin() / 10) { // staggered
-				
-				if (yard[(xCoords-1)+(yCoords*length)][s.getZ() + 1] == null) {
+			if (Math.floor(s.getXMin() / 10) != s.getXMin() / 10) { // staggered - WERKT NOG NIET HEHEXD
+				System.out.println("DEBUG - STAGGERED");
+				if (yard[(xCoords-1)+(yCoords*length)][s.getZ()] == null) {
 					vrij = true;
 				} else {
 					vrij = false;
-					if(maakVrij(yard[(xCoords-1)+(yCoords*length)][s.getZ() + 1])) {
-						moveItem(yard[(xCoords-1)+(yCoords*length)][s.getZ() + 1]);
+					if(maakVrij(yard[(xCoords-1)+(yCoords*length)][s.getZ()])) {
+						if(moveItem(yard[(xCoords-1)+(yCoords*length)][s.getZ()]))
+							yard[(xCoords-1)+(yCoords*length)][s.getZ()] = null;
 						vrij = true;
 					}
 				}
 				
-				if (yard[(xCoords-1)+(yCoords*length)+1][s.getZ() + 1] == null) {
+				if (yard[(xCoords-1)+(yCoords*length)+1][s.getZ()] == null) {
 					vrij = true;
 				} else {
 					vrij = false;
-					if(maakVrij(yard[(xCoords-1)+(yCoords*length)+1][s.getZ() + 1])) {
-						moveItem(yard[(xCoords-1)+(yCoords*length)+1][s.getZ() + 1]);
+					if(maakVrij(yard[(xCoords-1)+(yCoords*length)+1][s.getZ()])) {
+						if(moveItem(yard[(xCoords-1)+(yCoords*length)+1][s.getZ()]))
+							yard[(xCoords-1)+(yCoords*length)+1][s.getZ()] = null;
 						vrij = true;
 					}
 				}
 				
+				if(vrij) return true;
+				/*
 				if (yard[][s.getZ() + 1] == null) {
 					
 				} else {
@@ -213,11 +244,13 @@ public class Yard {
 						vrij = true;
 					}
 				}
-				
-				
+				*/
 			} else { // not staggered
-				if (yard[yCoords*length + xCoords][s.getZ() + 1] == null) {
-					if (s.getXMax()/10 == length || yard[(width*length-1) + (yCoords*(length-1)+(xCoords+1))][s.getZ() + 1] == null) {
+				System.out.print("DEBUG - yCoords: "+ yCoords + " xCoords: " + xCoords +" -> " + (yCoords*length + xCoords) + " Z level: " + (s.getZ()+1) + " || ");
+				System.out.println((yard[yCoords*length + xCoords][s.getZ()].getItem() == null));
+				if (yard[yCoords*length + xCoords][s.getZ()].getItem() == null) {
+					return true;
+					/*if (s.getXMax()/10 == length || yard[(width*length-1) + (yCoords*(length-1)+(xCoords+1))][s.getZ() + 1] == null) {
 						vrij = true;
 					} else {
 						vrij = false;
@@ -235,46 +268,121 @@ public class Yard {
 							vrij = true;
 						}
 					}
+					*/
 				} else {
 					vrij = false;
-					if(maakVrij(yard[yCoords*length + xCoords][s.getZ() + 1])) {
-						moveItem(yard[yCoords*length + xCoords][s.getZ() + 1]);
-						vrij = true;
+					if(s.getZ()+1 == height || maakVrijBoven(yard[yCoords*length + xCoords][s.getZ()+1])) {
+						//if(moveItem(yard[yCoords*length + xCoords][s.getZ()]))
+						//	yard[yCoords*length + xCoords][s.getZ()].setItem(null);
+						yard[yCoords*length + xCoords][s.getZ()].setItem(null);
+						return true;
 					}
 				}
 			}
 		} else
 			vrij = true;
+		return vrij;
+	}
+	
 
+	public boolean maakVrijBoven(Slot s) {
+		System.out.println("DEBUG - Trying to empty slot.. " + s.toString());
+		boolean vrij = false;
+		if (s.getZ() < height) {
+			int xCoords = s.getCenterX() / 10;
+			int yCoords = s.getCenterY() / 10;
+
+			if (Math.floor(s.getXMin() / 10) != s.getXMin() / 10) { // staggered - WERKT NOG NIET HEHEXD
+				System.out.println("DEBUG - STAGGERED");
+				if (yard[(xCoords-1)+(yCoords*length)][s.getZ()] == null) {
+					vrij = true;
+				} else {
+					vrij = false;
+					if(maakVrij(yard[(xCoords-1)+(yCoords*length)][s.getZ()])) {
+						if(moveItem(yard[(xCoords-1)+(yCoords*length)][s.getZ()]))
+							yard[(xCoords-1)+(yCoords*length)][s.getZ()] = null;
+						vrij = true;
+					}
+				}
+				
+				if (yard[(xCoords-1)+(yCoords*length)+1][s.getZ()] == null) {
+					vrij = true;
+				} else {
+					vrij = false;
+					if(maakVrij(yard[(xCoords-1)+(yCoords*length)+1][s.getZ()])) {
+						if(moveItem(yard[(xCoords-1)+(yCoords*length)+1][s.getZ()]))
+							yard[(xCoords-1)+(yCoords*length)+1][s.getZ()] = null;
+						vrij = true;
+					}
+				}
+				
+				if(vrij) return true;
+			} else { // not staggered
+				System.out.print("DEBUG - yCoords: "+ yCoords + " xCoords: " + xCoords +" -> " + (yCoords*length + xCoords) + " Z level: " + (s.getZ()+1) + " || ");
+				System.out.println((yard[yCoords*length + xCoords][s.getZ()].getItem() == null));
+				if (yard[yCoords*length + xCoords][s.getZ()].getItem() == null) {
+					return true;
+				} else {
+					vrij = false;
+					if(s.getZ()+1 == height || maakVrijBoven(yard[yCoords*length + xCoords][s.getZ()+1])) {
+						if(moveItem(yard[yCoords*length + xCoords][s.getZ()]))
+							yard[yCoords*length + xCoords][s.getZ()].setItem(null);
+						return true;
+					}
+				}
+			}
+		} else
+			vrij = true;
 		return vrij;
 	}
 
-	public void moveItem(Slot s) {
-
+	public boolean moveItem(Slot s) {
+		System.out.println("DEBUG - Moving item ("+s.getItem().getId()+")");
+		boolean succes = false;
+		for (int j = 0; j < slotList.size(); j++) {
+			Slot temp = slotList.get(j);
+			if (temp.getItem() == null) {
+				temp.setItem(s.getItem());
+				succes = true;
+				j = slotList.size() + 10;
+				itemIDList.put(temp.getItem().getId(), temp);
+				System.out.println("DEBUG - We found a suiteable slot! " + temp.toString());
+			}
+		}
+		return succes;
 	}
 
 	public boolean executeJob(Job j, String mode) {
 		boolean succes = false;
-
 		if (mode == "INPUT") {
-			succes = addItem(j.getItem());
+			System.out.println("TASKHANDLER - adding " +  j.getItem().toString());
+			succes = addItem(j.getItem());			
+			
+			if(!succes) {
+				System.out.println("We failed to handle item " + j.getItem().toString() + " (IN) for now, backlogging");
+				backlogIN.add(j);
+			} else 
+				System.out.println("We succeeded in handling (IN) item " + j.getItem().toString());
 		} else if (mode == "OUTPUT") {
+			System.out.println("TASKHANDLER - removing " +  j.getItem().toString());
 			succes = digItem(j.getItem());
+			
+			if(!succes) {
+				System.out.println("We failed to handle item " + j.getItem().toString() + " (OUT) for now, backlogging");
+				backlogOUT.add(j);
+			} else 
+				System.out.println("We succeeded in handling (OUT) item " + j.getItem().toString());
 		}
-
 		return succes;
-
 	}
 
 	public void printHash() {
 		// System.out.println(itemIDList);
 		System.out.println("Printing Hash Map!");
 		for (Integer id : itemIDList.keySet()) {
-
 			int slot = itemIDList.get(id).getId();
 			int container = itemIDList.get(id).getItem().getId();
 			System.out.println("Hash id:" + id + "\t Slot id:" + slot + "\t Container id:" + container);
-
 		}
 	}
 }
