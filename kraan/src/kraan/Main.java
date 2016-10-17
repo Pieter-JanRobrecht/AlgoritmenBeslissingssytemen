7ec65c8e81433713fe41e789cc9ee932fd8bdfd7
Pieter-Jan Robrecht <pieterjan.robrecht@student.kuleuven.be>
2016-10-17 17:30:05 +0200

    Init van de GUi

--- kraan/src/kraan/Main.java
+++ kraan/src/kraan/Main.java
@@ -1,24 +1,47 @@
 package kraan;
 
-import java.io.File;
-import java.io.FileReader;
-import java.net.URL;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Main {
+import java.io.File;
+import java.io.IOException;
+
+public class Main extends Application {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		 
         try {
-        	File file = new File("./data/1_50_50_10_FALSE_60_25_100.json");
+			//Misschien zorgt deze lijn voor problemen
+			//Zoja, pech gehad
+			File file = new File(Main.class.getClassLoader().getResource("1_50_50_10_FALSE_60_25_100.json").toURI());
+//        	File file = new File("./data/1_50_50_10_FALSE_60_25_100.json");
 
-        	
         	Problem uwProbleem = Problem.fromJson(file);
  
         	System.out.println(uwProbleem.toString());
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+		launch(args);
 	}
 
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			Parent root = (Parent) loader.load(getClass().getClassLoader().getResource("Sample.fxml").openStream());
+
+			primaryStage.setTitle("Kranen probleem");
+			primaryStage.setScene(new Scene(root));
+			primaryStage.show();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
---
+++ kraan/src/view/View.java
@@ -0,0 +1,14 @@
+package view;
+
+import java.util.Observable;
+import java.util.Observer;
+
+/**
+ * Created by Pieter-Jan on 17/10/2016.
+ */
+public class View implements Observer {
+    @Override
+    public void update(Observable o, Object arg) {
+
+    }
+}
