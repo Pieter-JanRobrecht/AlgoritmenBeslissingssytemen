cf5e59c39f396fd4a8fd5c4550a24db821e86fe7
Pieter-Jan Robrecht <pieterjan.robrecht@student.kuleuven.be>
2016-10-20 18:49:54 +0200

    Basis gui met controller

--- kraan/src/view/View.java
+++ kraan/src/view/View.java
@@ -3,10 +3,35 @@ package view;
 import java.util.Observable;
 import java.util.Observer;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TabPane;
+
+
 /**
  * Created by Pieter-Jan on 17/10/2016.
  */
 public class View implements Observer {
+
+    @FXML
+    private TabPane tabPane;
+
+    @FXML
+    void loadFileBigNietGeschrankt(ActionEvent event) {
+
+    }
+
+    @FXML
+    void loadFileBigSchrankt(ActionEvent event) {
+
+    }
+
+    @FXML
+    void loadFileSmall(ActionEvent event) {
+
+    }
+
+
     @Override
     public void update(Observable o, Object arg) {
 
