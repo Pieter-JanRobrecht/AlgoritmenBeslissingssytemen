83bdc0db95292c10c38078efaf315a2cb28bf595
Pieter-Jan Robrecht <pieterjan.robrecht@student.kuleuven.be>
2016-11-01 11:46:27 +0100

    merge deel 1

--- kraan/src/controller/Controller.java
+++ kraan/src/controller/Controller.java
@@ -10,40 +10,73 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.Observable;
+import java.io.FileWriter;
+import java.util.Arrays;
 
 /**
  * Created by Pieter-Jan on 20/10/2016.
  */
 public class Controller extends Observable {
+	
 
+	
     private Problem huidigProbleem;
 
     private int limit, inLimit, outLimit, klok;
 
     private Yard yard;
-
-    public Controller() {
+    FileWriter writer;
+    
+    public Controller() throws IOException {
+        try {
+            File hulp = new File(Main.class.getClassLoader().getResource("test.csv").toURI());
+            writer = new FileWriter(hulp);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
         if (huidigProbleem == null) {
             setFileSmall();
         }
-
+       // String csvFile = "./data/csv/test.csv";
+       // FileWriter writer = new FileWriter(csvFile);
+      // CSVUtils.writeLine(writer, Arrays.asList("gID","T","x","y","itemInCraneID"));
+       // writer.flush();
         reset();
     }
 
-    public void doStep(boolean notify) {
+    public void doStep(boolean notify) throws IOException {
+    	
+
+       if(klok==0) {CSVUtils.writeLine(writer, Arrays.asList("gID","T","x","y","itemInCraneID")); writer.flush();}
+       
+            
+        
+    	
         if (klok < inLimit) {
             yard.executeJob(huidigProbleem.getInputJobSequence().get(klok), "INPUT");
 //            yard.printOutYard();
+            
+         CSVUtils.writeLine(writer, Arrays.asList(""+Yard.csvparam1[0],""+klok,""+Yard.csvparam1[1],""+Yard.csvparam1[2],""+Yard.csvparam1[3]));
+         writer.flush();
         }
         if (klok < outLimit) {
             yard.executeJob(huidigProbleem.getOutputJobSequence().get(klok), "OUTPUT");
 //            yard.printOutYard();
+            CSVUtils.writeLine(writer, Arrays.asList(""+Yard.csvparam1[0],""+klok,""+Yard.csvparam1[1],""+Yard.csvparam1[2],""+Yard.csvparam1[3]));
+            writer.flush();
         }
+        
         klok++;
         if (notify) {
             setChanged();
             notifyObservers();
+            
+            
         }
+        
+       if (klok >= inLimit && klok>=outLimit){ writer.close();}
+        
+        
 
         //TODO back log moet vervangen worden met code
 //        System.out.println("Done!");
